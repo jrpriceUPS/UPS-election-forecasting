77f84a950ac40cd69246aadcf8fc95ec632ee9c4
Haley Reed <hireed@pugetsound.edu>
2020-07-02 12:32:43 -0700

    Added Script to Explore Party Error v. Margins
    Added an Rscript to explore how dem error/bias differs from under/overestimation of the margin between the two parties.

---
+++ RScripts/Exploration of Margin v. Party Error.R
@@ -0,0 +1,10 @@
+#Margin vs. Dem Error
+#Haley Reed
+#July 2nd 2020
+
+#Is democratic year lean = - Rebuplican year Lean?
+
+mydata=read.csv("Data/raw-polls_538_cleaned.csv")
+head(mydata[,33:34])
+
+#so far it does not look like it
