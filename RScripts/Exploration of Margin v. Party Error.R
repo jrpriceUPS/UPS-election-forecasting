1352a12a4c6ab3d4d9f1d1320077a36ecb7bb0e6
Haley Reed <hireed@pugetsound.edu>
2020-07-06 15:32:41 -0700

    Metric-Metric Bayes Analysis
    Added ymet-xmet bayes analysis for repBias~demBias and bias~demBias.

--- RScripts/Exploration of Margin v. Party Error.R
+++ RScripts/Exploration of Margin v. Party Error.R
@@ -176,4 +176,54 @@ abline(RelationshipBetween1)
 
 
 
+# Example for Jags-Ymet-Xmet-Mrobust.R 
+#------------------------------------------------------------------------------- 
+# Optional generic preliminaries:
+graphics.off() # This closes all of R's graphics windows.
+
+#------------------------------------------------------------------------------- 
+# Load data file and specity column names of x (predictor) and y (predicted):
+myData = myDataMyPollsters
+xName = "demBias" ; yName = "repBias"
+fileNameRoot = "Markdown/Figures/DoubleOneMetric-ErrorandBias-Jags-" 
+fileNameRootSim = "Simulations/DoubleOneMetric-ErrorandBias-Jags-" 
+
+xName = "demBias" ; yName = "bias"
+fileNameRoot = "Markdown/Figures/DoubleOneMetric-ErrorandBias-Jags-Margin" 
+fileNameRootSim = "Simulations/DoubleOneMetric-ErrorandBias-Jags-Margin" 
+#............................................................................
+
+#............................................................................
+graphFileType = "png" 
+#------------------------------------------------------------------------------- 
+# Load the relevant model into R's working memory:
+source("RScripts/Jags-Ymet-Xmet-Mrobust.R")
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+#startTime = proc.time()
+mcmcCoda = genMCMC( data=myData , xName=xName , yName=yName , 
+                    numSavedSteps=20000 , saveName=fileNameRootSim )
+#stopTime = proc.time()
+#duration = stopTime - startTime
+#show(duration)
+#------------------------------------------------------------------------------- 
+# Display diagnostics of chain, for specified parameters:
+parameterNames = varnames(mcmcCoda) # get all parameter names
+for ( parName in parameterNames ) {
+  diagMCMC( codaObject=mcmcCoda , parName=parName , 
+            saveName=fileNameRoot , saveType=graphFileType )
+}
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( mcmcCoda , 
+                        compValBeta1=0.0 , ropeBeta1=c(-0.5,0.5) ,
+                        saveName=fileNameRootSim )
+show(summaryInfo)
+# Display posterior information:
+plotMCMC( mcmcCoda , data=myData , xName=xName , yName=yName , 
+         # compValBeta1=0.0 , ropeBeta1=c(-0.5,0.5) ,
+          pairsPlot=TRUE , showCurve=FALSE ,
+          saveName=fileNameRoot , saveType=graphFileType )
+#------------------------------------------------------------------------------- 
+
 
