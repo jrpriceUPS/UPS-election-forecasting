37bcc40ad1105c1823d3f10fb17ff1a6f70d0a3b
Haley Reed <hireed@pugetsound.edu>
2020-07-28 14:29:55 -0700

    Just Sample Size & delMode
    Created version of model with just sample size and delivery mode

--- RScripts/TesterforLimitedVariables.R
+++ RScripts/TesterforLimitedVariables.R
@@ -543,3 +543,161 @@ lattice::densityplot(~bias, data = subdata, main = "2000 LV v. Non LV Comparsion
 
 
 
+
+
+#Look at just delMode and sample size. No transparency or LV/RV Impact
+#ANCOVA for Weight of Poll - Example Script
+#06/30/2020
+
+graphics.off() # This closes all of R's graphics windows.
+#rm(list=ls())  # Clear all of R's memory!
+
+#load the cleaned data
+#mydata=read.csv("Data/raw-polls_538_cleaned.csv")
+mydata=read.csv("Data/raw-polls_538_weekprior.csv")
+#remove mail and landline (3 total polls)
+mydata=mydata[mydata$delMode!="Landline",]
+mydata=mydata[mydata$delMode!="Mail",]
+
+
+#Order the data
+
+newdata <- mydata[order(mydata$race_id),]
+newdata2 = data.frame(newdata)
+
+#create small data frame for reference
+races = unique(newdata$race_id)
+onlyUnique=(data.table::setDT(newdata2)[,.SD[which.max(cand1_actual)],keyby=race_id])
+actual = onlyUnique$cand1_actual
+raceFullName=unique(newdata$race)
+refdataframe=data.frame(races,raceFullName,actual) 
+
+for (i in 1:nrow(refdataframe)){
+  
+  myFullName=refdataframe[i,2]
+  
+  myFullName= stringr::str_replace_all(myFullName, "_", " ")
+  refdataframe[i,2]=myFullName
+}
+
+# refdataframe=data.frame(races,raceFullName,actual) 
+
+#create the whichrace list. 
+
+
+# whichrace <- vector(mode = "numeric", 0)
+#  for (i in unique(newdata$race_id)){
+#    subsetrace = subset (newdata, race_id==i)
+#    counter=nrow(subsetrace)
+#    whichrace = rlist::list.append(whichrace, counter)
+#  }
+
+
+#create list to feed info with
+
+predictorsframe = newdata[,c("race_id","cand1_actual", "cand1_pct",
+                             "delMode","transparency", "samplesize","LV")]
+
+
+myDataFrame=predictorsframe
+
+
+#Less Options for Del Mode is Helpful:
+for (i in 1:nrow(myDataFrame)){
+  myMode=myDataFrame[i,4]
+  if(myMode=="IVR/Online"||myMode=="IVR/Online/Live"||myMode=="IVR/Online/Text"||myMode=="IVR/Online/Live/Text"||myMode=="	IVR/Online/Text"||myMode=="IVR/Text"){
+    myDataFrame[i,4]="Online"
+  }
+  if(myMode=="Live*"||myMode=="Live/Text"||myMode=="Online/Live"||myMode=="IVR/Live"){
+    myDataFrame[i,4]="Live"
+  }
+}
+
+
+
+predictorsframe=myDataFrame
+#which race indexing:
+whichrace=match(unique(predictorsframe$race_id), predictorsframe$race_id)
+whichrace=whichrace-1
+whichrace=c(whichrace,nrow(newdata))
+
+
+fileNameRootSim = "Simulations/Weight-SampleSizeDelMode" 
+fileNameRoot = "Markdown/Figures/Weight-SampleSizeDelMode" 
+graphFileType = "png" 
+
+myDataFrame$samplesize =myDataFrame $ samplesize/1000
+#myDataFrame$samplesize = myDataFrame$log(samplesize)
+
+#MarginOfError = sqrt(.25/myDataFrame$samplesize)*100
+#myDataFrame$samplesize =MarginOfError
+#------------------------------------------------------------------------------- 
+# Load the relevant model into R's working memory:
+#Worked without LV.
+source("RScripts/WeightedSampleSizeandDelMode.R")
+#------------------------------------------------------------------------------- 
+# Generate the MCMC chain:
+mcmcCodaSSDM = genMCMC( refFrame=refdataframe, datFrmPredictor=myDataFrame, pollName="cand1_pct" ,
+                    actualName="actual", 
+                    #LVName="LV" ,
+                    delModeName="delMode" ,  transparencyName="transparency", 
+                    samplesizeName ="samplesize", raceIDName = "races", whichrace=whichrace,
+                    numSavedSteps=11000 , thinSteps=10 , saveName=fileNameRootSim )
+#------------------------------------------------------------------------------- 
+# Display diagnostics of chain, for specified parameters:
+parameterNames = varnames(mcmcCodaSSDM) 
+show( parameterNames ) # show all parameter names, for reference
+for ( parName in c("actualSpread",   
+                   "delModeImpact[1]", "samplesizeImpact" , "mu[1]") ) {
+  diagMCMC( codaObject=mcmcCodaSSDM , parName=parName , 
+            saveName=fileNameRoot , saveType=graphFileType )
+}
+#------------------------------------------------------------------------------- 
+# Get summary statistics of chain:
+summaryInfo = smryMCMC( codaSamples=mcmcCodaSSDM , datFrm=myDataFrame , delModeName="delMode" , #LVName="LV" ,
+                        transparencyName="transparency", 
+                        samplesizeName ="samplesize", 
+                        saveName=fileNameRoot )
+show(summaryInfo)
+# Display posterior information: At this point just for delMode and sample size.
+#plotPosteriorPredictive( mcmcCodaSSDM , datFrm=myDataFrame , 
+#      saveName=fileNameRoot , saveType=graphFileType )
+#------------------------------------------------------------------------------- 
+#plot the posterior predictive distrubtions
+
+plotPosteriorPredictive(codaSample=mcmcCodaSSDM, refFrame=refdataframe, datFrmPredictor = myDataFrame, 
+                        pollName = "cand1_pct", raceIDName="races", raceplots=1:10, whichrace=whichrace, saveName=fileNameRoot , 
+                        saveType=graphFileType)
+
+meanPosterior(codaSample=mcmcCodaSSDM, refFrame=refdataframe, datFrmPredictor = myDataFrame, 
+              pollName = "cand1_pct", raceIDName="races", raceplots=1:10, whichrace=whichrace, saveName=fileNameRoot , 
+              saveType=graphFileType)
+#plot the LV distribution
+# plotLVPosterior(codaSample=mcmcCodaSSDM,  datFrmPredictor = myDataFrame , saveName=fileNameRoot , 
+#                 saveType=graphFileType)
+
+#plot delModeImpact Posterior
+plotdelModePosterior(codaSample=mcmcCodaSSDM,  datFrmPredictor = myDataFrame , saveName=fileNameRoot , 
+                     saveType=graphFileType)
+
+#plot Transparenct Posterior
+plotTransparencyPosterior(codaSample=mcmcCodaSSDM,  datFrmPredictor = myDataFrame , saveName=fileNameRoot , 
+                          saveType=graphFileType)
+
+#plot the samplesize 
+plotSampleSizePosterior(mcmc, datFrm=myDataFrame,  saveName=fileNameRoot , 
+                        saveType=graphFileType, title="Sample Size Impact ")
+
+#plot SampleSize Prior 
+
+agammaShRa = unlist( gammaShRaFromModeSD( mode=sd(actual)/2 , sd=2*sd(actual) ) )
+
+#X <- rgamma( shape=1.2832, scale=0.0624, n=2750)
+X <- rgamma( shape=1.2832, scale=0.0624, n=11000)
+mean(X)
+mode(X)
+
+hist(X,prob=T,main='Gamma Prior', breaks=47)
+#lines(density(X),col='red',lwd=2)
+
+
