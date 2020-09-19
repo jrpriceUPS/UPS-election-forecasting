7d0a5b793453f9fb95fe0fbc54259d5051c3fb8c
Haley Reed <hireed@pugetsound.edu>
2020-09-18 21:13:20 -0700

    One DataFrame
    Updated the new data clean up file to take the 3 economist data files from 2008, 2012, and 2016 into 1 dataframe.

--- RScripts/dataCleanUpEconomist.R
+++ RScripts/dataCleanUpEconomist.R
@@ -25,8 +25,40 @@ pollingData2008$year = "2008"
 pollingData2012$year = "2012"
 pollingData2016$year = "2016"
 
-pollingData2016Con <- pollingData2016[state,pollster,number.of.observations,start.date,
-                                      end.date,trump,clinton,other,undecided,mode,
-                                      population,pollsterFullName,year]
+#create a 2016 dataframe that has the same variables as 2008 and 2012.
+pollingData2016Con <- data.frame(pollingData2016$state, pollingData2016$pollster,
+                                 pollingData2016$number.of.observations, pollingData2016$start.date,
+                                 pollingData2016$end.date, pollingData2016$trump, pollingData2016$clinton,
+                                 pollingData2016$other,pollingData2016$undecided, pollingData2016$mode,
+                                 pollingData2016$population,pollingData2016$pollsterFullName,
+                                 pollingData2016$year)
 
-newFrame = rbind(pollingData2008,pollingData2012,pollingData2016)
+#correct the names of the variables
+pollingData2016Con$state=pollingData2016Con$pollingData2016.state
+pollingData2016Con$pollster=pollingData2016Con$pollingData2016.pollster
+pollingData2016Con$number.of.observations=pollingData2016Con$pollingData2016.number.of.observations
+pollingData2016Con$start.date=pollingData2016Con$pollingData2016.start.date
+pollingData2016Con$end.date=pollingData2016Con$pollingData2016.end.date
+pollingData2016Con$other=pollingData2016Con$pollingData2016.other
+pollingData2016Con$undecided=pollingData2016Con$pollingData2016.undecided
+pollingData2016Con$mode=pollingData2016Con$pollingData2016.mode
+pollingData2016Con$population=pollingData2016Con$pollingData2016.population
+pollingData2016Con$pollsterFullName=pollingData2016Con$pollingData2016.pollsterFullName
+pollingData2016Con$year=pollingData2016Con$pollingData2016.year
+pollingData2016Con$democrat=pollingData2016Con$pollingData2016.clinton
+pollingData2016Con$republican=pollingData2016Con$pollingData2016.trump
+
+pollingData2016Con = pollingData2016Con[,-(1:13)]  
+
+#standardize the vote share variables - for 2008
+pollingData2008$democrat=pollingData2008$obama           
+pollingData2008$republican=pollingData2008$mccain           
+pollingData2008 <- pollingData2008[,-(6:7)]  
+
+#for 2012...
+pollingData2012$democrat=pollingData2012$obama           
+pollingData2012$republican=pollingData2012$romney           
+pollingData2012 <- pollingData2012[,-(6:7)]  
+
+
+newFrame = rbind(pollingData2008,pollingData2012,pollingData2016Con)
